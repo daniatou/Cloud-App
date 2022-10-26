de2fe3987acd52c5399b1265e818e0c215e22bc3
daniatoudjibril <daniatoudjibril@gmail.com>
2022-10-27 01:27:43 +0200

    baniere

---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { CloudIcon, ServerStackIcon, PaperAirplaneIcon, ServerIcon } from '@heroicons/react/24/outline'
+import bgImage from '../images/cyber-bg.png'
+
+
+export const Hero = () => {
+    return (
+        <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
+            <div className="grid sm:grid-cols-2 max-w-[1240px] mx-auto my-auto">
+                <div className="flex flex-col justify-center md:items-start w-full px-2">
+                    <p className='text-2xl'>Unique Sequencies & Production</p>
+                    <h1 className='py-4 text-5xl md:text-7xl'>
+                        Cloud Management
+                    </h1>
+                    <p className='text-2xl '>
+                        This is Our Tech Brand
+                    </p>
+                    <button className='sm: w-[60%] my-4'>
+                        Get Started
+                    </button>
+                </div>
+                <div className='w-full'>
+                    <img src={bgImage} alt='Illustration baniere ' />
+                </div>
+                <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[3%]
+                   mx-1 md:left-[20%]  transform md:translate-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+                    <p>Data Services</p>
+                    <div className='flex justify-between flex-wrap px-4'>
+                        <p className='flex px-4 py-2 text-slate-500'> <CloudIcon className='h-6 text-indigo-600 px-2' />   App Security</p>
+                        <p className='flex px-4 py-2 text-slate-500'> <ServerStackIcon className='h-6 text-indigo-600 px-2' />  Dashboard Designer</p>
+                        <p className='flex px-4 py-2 text-slate-500'> <PaperAirplaneIcon className='h-6 text-indigo-600 px-2' /> Cloud Data</p>
+                        <p className='flex px-4 py-2 text-slate-500'> <CloudIcon className='h-6 text-indigo-600 px-2' /> API</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
 
 
     return (
-        <div className="navbar bg-zinc-200 h-[80px] w-full fixed z-10 ">
+        <div className="navbar bg-zinc-200 h-[80px] w-full fixed z-10 shadow-lg ">
             <div className="px-2 flex justify-between items-center w-full h-full">
                 <h1 className=' logo text-3xl font-bold ml-3 sm:text-4xl '>
                     BRAND.
@@ -30,7 +30,7 @@ export const Navbar = () => {
                     {!nav ? <Bars3Icon className='w-6' /> : <XMarkIcon className='w-6' />}
                 </div>
             </div>
-            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full m-0 p-3'}>
+            <ul className={!nav ? 'hidden' : 'absolute bg-zinc-200 w-full m-0 p-3 md:hidden'}>
                 <li className='border-b-2 border-zinc-300 w-full'>Home</li>
                 <li className='border-b-2 border-zinc-300 w-full'>About</li>
                 <li className='border-b-2 border-zinc-300 w-full'>Support</li>
